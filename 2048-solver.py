2acf8cbca8e6d67441da61121d08f973911ddf0e
Pigs1 <pigsgoheehawfilms@gmail.com>
2023-05-16 10:03:22 -0500

    simple move left func

--- 2048-solver.py
+++ 2048-solver.py
@@ -1,1 +1,25 @@
+import random
 
+board = [[0,2,0,0],
+         [0,0,2,0],
+         [2,0,0,2],
+         [0,0,0,2]]
+
+def Move_Left():
+    global board
+    for row in board:
+        consecutive_zeros = 0
+        row_index = 0
+
+        for value in row:
+            if value == 0:
+                consecutive_zeros += 1
+            elif consecutive_zeros != 0:
+                row[row_index - consecutive_zeros] = row[row_index]
+                row[row_index] = 0
+                consecutive_zeros = 0
+            row_index += 1
+        print(row)
+    return board
+
+Move_Left()
