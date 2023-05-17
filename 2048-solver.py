169a8fba525cf190abea5a4c1f1b0c97284d6185
Pigs1 <pigsgoheehawfilms@gmail.com>
2023-05-17 08:07:06 -0500

    comments, print board

--- 2048-solver.py
+++ 2048-solver.py
@@ -5,21 +5,36 @@ board = [[0,2,0,0],
          [2,0,0,2],
          [0,0,0,2]]
 
-def Move_Left():
+def print_board():
     global board
     for row in board:
+        print(row)
+    return True
+
+def Move_Left():
+    global board
+    # iterate through each row
+    for row in board:
+        # reset vars for each row
         consecutive_zeros = 0
         row_index = 0
-
+        # iterate through each value in the row
         for value in row:
+            # count zeroes in a row
             if value == 0:
                 consecutive_zeros += 1
+            # if the value is not equal to 0, and there have been zeroes before it
             elif consecutive_zeros != 0:
+                # shift the non-zero value to the left
                 row[row_index - consecutive_zeros] = row[row_index]
+                # replace the non-zero value with zero
                 row[row_index] = 0
+                # reset consecutive zeroes
                 consecutive_zeros = 0
+            # to track which index it is checking
             row_index += 1
-        print(row)
-    return board
+    print_board()
+    
+    return
 
 Move_Left()
