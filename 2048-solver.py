916d4255bfff52ac1708cdb5b74e083602f9d56a
Pigs1 <pigsgoheehawfilms@gmail.com>
2023-05-19 08:21:39 -0500

    Movement fixes

--- 2048-solver.py
+++ 2048-solver.py
@@ -1,9 +1,9 @@
 import random
 
-board = [[0,2,0,0],
-         [0,0,2,0],
-         [2,0,0,2],
-         [0,0,0,2]]
+board = [[0,2,0,2],
+         [0,0,0,0],
+         [0,0,0,0],
+         [0,0,0,0]]
 
 def print_board():
     global board
@@ -17,12 +17,12 @@ def Move(dir):
     for row in board:
         # reset vars for each row
         consecutive_zeros = 0
-        row_index = 0
+
         # iterate through each value in the row
         if dir == "left":
-            for value in range(4):
+            for row_index in range(4):
                 # count zeroes in a row
-                if row[value] == 0:
+                if row[row_index] == 0:
                     consecutive_zeros += 1
                 # if the value is not equal to 0, and there have been zeroes before it
                 elif consecutive_zeros != 0:
@@ -30,56 +30,69 @@ def Move(dir):
                     row[row_index - consecutive_zeros] = row[row_index]
                     # replace the non-zero value with zero
                     row[row_index] = 0
+                    try:
+                        if row[row_index + 1] != 0:
+                            # shift the non-zero value to the left
+                            row[row_index + 1 - consecutive_zeros] = row[row_index + 1]
+                            # replace the non-zero value with zero
+                            row[row_index + 1] = 0
+                    except:
+                        consecutive_zeros = 0
+                    
                     # reset consecutive zeroes
-                    consecutive_zeros = 0
-                
-                # to track which index it is checking
-                row_index += 1
+                    consecutive_zeros = 1
 
         elif dir == "right":
-            row_index = 3
-            for value in range(3,-1,-1):
+            for row_index in range(3,-1,-1):
                 # count zeroes in a row
-                if row[value] == 0:
+                if row[row_index] == 0:
                     consecutive_zeros += 1
                 # if the value is not equal to 0, and there have been zeroes before it
                 elif consecutive_zeros != 0:
-                    # shift the non-zero value to the left
+                    # shift the non-zero value to the right
                     row[row_index + consecutive_zeros] = row[row_index]
                     # replace the non-zero value with zero
                     row[row_index] = 0
-                    # reset consecutive zeroes
-                    consecutive_zeros = 0
+                    try:
+                        if row[row_index - 1] != 0:
+                            # shift the non-zero value to the right
+                            row[row_index - 1 + consecutive_zeros] = row[row_index - 1]
+                            # replace the non-zero value with zero
+                            row[row_index - 1] = 0
+                    except:
+                        consecutive_zeros = 0
 
-                # to track which index it is checking
-                row_index -= 1
+                    # reset consecutive zeroes
+                    consecutive_zeros = 1
 
         elif dir == "down":
             for col in range(4):
-                col_index = 0
                 temp_list = []
                 consecutive_zeros = 0
+                # create a temporary list of all the values in a column
                 for row2 in board:
                     temp_list.append(row2[col])
 
-                col_index = 3
-                for value in range(3,-1,-1):
+                for col_index in range(3,-1,-1):
                     # count zeroes in a row
-                    if temp_list[value] == 0:
+                    if temp_list[col_index] == 0:
                         consecutive_zeros += 1
                     # if the value is not equal to 0, and there have been zeroes before it
                     elif consecutive_zeros != 0:
-                        # shift the non-zero value to the left
+                        # shift the non-zero value to the right in temp list
                         temp_list[col_index + consecutive_zeros] = temp_list[col_index]
                         # replace the non-zero value with zero
                         temp_list[col_index] = 0
-                        # reset consecutive zeroes
-                        consecutive_zeros = 0
-                    
-                    # to track which index it is checking
-                    col_index -= 1
 
-                
+                        if temp_list[col_index - 1] != 0:
+                            # shift the non-zero value to the right in temp list
+                            temp_list[col_index - 1 + consecutive_zeros] = temp_list[col_index - 1]
+                            # replace the non-zero value with zero
+                            temp_list[col_index - 1] = 0
+
+                        # reset consecutive zeroes
+                        consecutive_zeros = 1
+    
                 for row3 in range(4):
                     board[row3][col] = temp_list[row3]
         
@@ -89,24 +102,35 @@ def Move(dir):
                 temp_list = []
                 consecutive_zeros = 0
 
+                # create a temporary list of all the values in a column
                 for row2 in board:
                     temp_list.append(row2[col])
 
-                for value in range(4):
+                for col_index in range(4):
                     # count zeroes in a row
-                    if temp_list[value] == 0:
+                    if temp_list[col_index] == 0:
                         consecutive_zeros += 1
                     # if the value is not equal to 0, and there have been zeroes before it
                     elif consecutive_zeros != 0:
-                        # shift the non-zero value to the left
+                        # shift the non-zero value to the left in temp list
                         temp_list[col_index - consecutive_zeros] = temp_list[col_index]
                         # replace the non-zero value with zero
                         temp_list[col_index] = 0
+                        try:
+                            if temp_list[col_index + 1] != 0:
+                                # shift the non-zero value to the left in temp list
+                                row[col_index + 1 - consecutive_zeros] = row[col_index + 1]
+                                # replace the non-zero value with zero
+                                row[col_index + 1] = 0
+                        except:
+                            consecutive_zeros = 0
+
                         # reset consecutive zeroes
-                        consecutive_zeros = 0
+                        consecutive_zeros = 1
                     
                     # to track which index it is checking
                     col_index += 1
+                    
 
                 
                 for row3 in range(4):
@@ -135,6 +159,16 @@ def add_value():
 
     return
 
-Move("left")
-Move("right")
-print_board()
+running = True
+
+while running == True:
+    cmd = input()
+    if cmd == "w":
+        Move("up")
+    elif cmd == "s":
+        Move("down")
+    elif cmd == "a":
+        Move("left")
+    elif cmd == "d":
+        Move("right")
+    print_board()
